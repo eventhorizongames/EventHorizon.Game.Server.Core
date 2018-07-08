d53ead8f254eafe8a84e742cf67fd04f9d536d46
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2018-07-07 19:19:12 -0500

    Updates.

--- Account/Details/Handler/AccountDetailsHandler.cs
+++ Account/Details/Handler/AccountDetailsHandler.cs
@@ -9,16 +9,19 @@ using MediatR;
 using EventHorizon.Game.Server.Core.Player.Events.Details;
 using EventHorizon.Game.Server.Core.Player.Events.Create;
 using EventHorizon.Game.Server.Core.Player.Events.Zone;
+using Microsoft.Extensions.Logging;
 
 namespace EventHorizon.Game.Server.Core.Account.Details.Handler
 {
     public class AccountDetailsHandler : IRequestHandler<AccountDetailsEvent, AccountDetails>
     {
+        readonly ILogger _logger;
         private readonly IMediator _mediator;
         private readonly IAccountRepository _accountRepository;
 
-        public AccountDetailsHandler(IMediator mediator, IAccountRepository accountRepository)
+        public AccountDetailsHandler(ILogger<AccountDetailsHandler> logger, IMediator mediator, IAccountRepository accountRepository)
         {
+            _logger = logger;
             _mediator = mediator;
             _accountRepository = accountRepository;
         }
@@ -30,11 +33,13 @@ namespace EventHorizon.Game.Server.Core.Account.Details.Handler
 
         private async Task<AccountDetails> Map(AccountEntity entity)
         {
+            _logger.LogInformation(entity.Id);
             // Get Player Details
             var playerDetails = await _mediator.Send(new PlayerGetDetailsEvent
             {
                 Id = entity.Id
             });
+            _logger.LogInformation(playerDetails.IsNew().ToString());
             if (playerDetails.IsNew())
             {
                 playerDetails = await _mediator.Send(new PlayerCreateNewEvent
@@ -42,11 +47,13 @@ namespace EventHorizon.Game.Server.Core.Account.Details.Handler
                     Id = entity.Id
                 });
             }
+            _logger.LogInformation(playerDetails.Id);
             // Get the Player's Zone
             var zone = await _mediator.Send(new ZoneDetailsEvent
             {
                 Id = playerDetails.Position.CurrentZone,
             });
+            _logger.LogInformation(zone.Id);
             if (!zone.IsFound())
             {
                 playerDetails = await _mediator.Send(new PlayerFindNewZoneEvent
@@ -54,6 +61,7 @@ namespace EventHorizon.Game.Server.Core.Account.Details.Handler
                     Player = playerDetails
                 });
             }
+            _logger.LogInformation(playerDetails.Position.CurrentZone);
             return new AccountDetails
             {
                 Id = entity.Id,
