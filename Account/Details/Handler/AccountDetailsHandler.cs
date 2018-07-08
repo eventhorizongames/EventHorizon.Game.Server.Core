1c2f4b540f625a9c60397003310401acb6c4d774
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2018-07-07 19:29:27 -0500

    Remove not needed logging.

--- Account/Details/Handler/AccountDetailsHandler.cs
+++ Account/Details/Handler/AccountDetailsHandler.cs
@@ -15,13 +15,11 @@ namespace EventHorizon.Game.Server.Core.Account.Details.Handler
 {
     public class AccountDetailsHandler : IRequestHandler<AccountDetailsEvent, AccountDetails>
     {
-        readonly ILogger _logger;
         private readonly IMediator _mediator;
         private readonly IAccountRepository _accountRepository;
 
         public AccountDetailsHandler(ILogger<AccountDetailsHandler> logger, IMediator mediator, IAccountRepository accountRepository)
         {
-            _logger = logger;
             _mediator = mediator;
             _accountRepository = accountRepository;
         }
@@ -33,13 +31,11 @@ namespace EventHorizon.Game.Server.Core.Account.Details.Handler
 
         private async Task<AccountDetails> Map(AccountEntity entity)
         {
-            _logger.LogInformation(entity.Id);
             // Get Player Details
             var playerDetails = await _mediator.Send(new PlayerGetDetailsEvent
             {
                 Id = entity.Id
             });
-            _logger.LogInformation(playerDetails.IsNew().ToString());
             if (playerDetails.IsNew())
             {
                 playerDetails = await _mediator.Send(new PlayerCreateNewEvent
@@ -47,13 +43,11 @@ namespace EventHorizon.Game.Server.Core.Account.Details.Handler
                     Id = entity.Id
                 });
             }
-            _logger.LogInformation(playerDetails.Id);
             // Get the Player's Zone
             var zone = await _mediator.Send(new ZoneDetailsEvent
             {
                 Id = playerDetails.Position.CurrentZone,
             });
-            _logger.LogInformation(zone.Id);
             if (!zone.IsFound())
             {
                 playerDetails = await _mediator.Send(new PlayerFindNewZoneEvent
@@ -61,7 +55,6 @@ namespace EventHorizon.Game.Server.Core.Account.Details.Handler
                     Player = playerDetails
                 });
             }
-            _logger.LogInformation(playerDetails.Position.CurrentZone);
             return new AccountDetails
             {
                 Id = entity.Id,
