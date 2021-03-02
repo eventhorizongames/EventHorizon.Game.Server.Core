cc1027a864bab8c2ef93e90d064db5635cf400ff
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-03-01 20:50:42 -0600

    Implemented Serilog All Server based services will now log to Elasticsearch using Serilog.
    Update Cloud Platform for Game Platform Deployment
    Added Serilog appsettings settings.
    Added Elasticsearch URL appsettings.
    Added OwnerDetails appsettings.
    Updated Locale Infrastructure for Cloud Deployments.
    
    Update Serilog Versions for Zone Service.
    Update Serilog Versions for Cloud Platform.
    
    Added Serilog to Player Service.
    Added Serilog to Agent Service.
    Added Serilog to Core Service.
    
    Removed ACR build steps.
    
    84C786D6-4218-49D0-9589-7BCE095AC999

--- src/EventHorizon.Game.Server.Core/Program.cs
+++ src/EventHorizon.Game.Server.Core/Program.cs
@@ -1,7 +1,11 @@
 ﻿namespace EventHorizon.Game.Server.Core
 {
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Sinks.Elasticsearch;
+    using System;
 
     public class Program
     {
@@ -15,6 +19,53 @@
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    webBuilder.UseSerilog((ctx, cfg) => cfg
+                        .Enrich.WithProperty("EnvironmentName", ctx.HostingEnvironment.EnvironmentName)
+                        .Enrich.WithProperty("PlatformId", ctx.Configuration["OwnerDetails:PlatformId"])
+                        .Enrich.WithProperty("ServiceName", "Core")
+                        .ReadFrom.Configuration(ctx.Configuration)
+                        .ConfigureElasticsearchLogging(ctx)
+                    );
                 });
+    }
+
+    public static class SerilogElasticsearchExtensions
+    {
+        public static LoggerConfiguration ConfigureElasticsearchLogging(
+            this LoggerConfiguration loggerConfig,
+            WebHostBuilderContext context
+        )
+        {
+            if (context.Configuration.GetValue<bool>("Serilog:Elasticsearch:Enabled"))
+            {
+                var sinkOptions = new ElasticsearchSinkOptions(
+                    new Uri(
+                        context.Configuration["Elasticsearch:Uri"]
+                    )
+                )
+                {
+                    ModifyConnectionSettings = conn =>
+                    {
+                        conn.BasicAuthentication(
+                            context.Configuration["Elasticsearch:Username"],
+                            context.Configuration["Elasticsearch:Password"]
+                        );
+                        return conn;
+                    }
+
+                };
+                context.Configuration.GetSection(
+                    "Serilog:Elasticsearch"
+                ).Bind(
+                    sinkOptions
+                );
+                return loggerConfig.WriteTo.Elasticsearch(
+                    sinkOptions
+                );
+            }
+
+            return loggerConfig;
+        }
     }
 }
